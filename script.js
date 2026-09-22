b8b3a5a6a55613bbb06bd8413acf2bfaf3e33b88
sanabriaq <sanabriajoaopedro15@gmail.com>
2026-09-22 12:32:48 -0300

    adiciona comentários nos arquivos html e js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-//CALCULADORA IPHONE//
-const display = document.getElementById("display");
-
-function appendToDisplay(input) {
-    display.value += input;
+//CALCULADORA IPHONE
+const display = document.getElementById("display"); //adiciona uma referência ao elemento de exibição da calculadora
+function appendToDisplay(input) { //adiciona o valor do botão clicado ao display da calculadora
+    display.value += input; //adiciona o valor do botão clicado ao display da calculadora
 }
 
-function clearDisplay() {
-    display.value = "";
+function clearDisplay() { //limpa o display da calculadora
+    display.value = ""; //limpa o display da calculadora
 }
 
-function calculate() {
+function calculate() { //realiza o cálculo da expressão no display da calculadora 
     try {
-        display.value = eval(display.value);
+        display.value = eval(display.value); // mostra o resultado do cálculo no display da calculadora
     }
-    catch (error) {
-        display.value = "Error";
+    catch (error) { //caso ocorra algum erro na avaliação da expressão, exibe "Error" no display da calculadora
+        display.value = "Error"; // exbe "Error" no display da calculadora
     }
 }
